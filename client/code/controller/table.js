464a9dcb23ff4b9d28ab52d0ac5a16b902fcfe54
freezy <freezy@xbmc.org>
2014-03-24 22:09:06 +0100

    Added Magnific Popup integration.

--- client/code/controller/table.js
+++ client/code/controller/table.js
@@ -11,5 +11,26 @@ ctrl.controller('TableController', function($scope, $http, $routeParams) {
 		table.lastrelease = new Date(table.lastrelease).getTime();
 
 		$scope.table = table;
+		setTimeout(function() {
+			$('.image-link').magnificPopup({
+				type: 'image',
+				removalDelay: 300,
+				mainClass: 'mfp-with-zoom',
+				zoom: {
+					enabled: true,
+
+					duration: 300, // duration of the effect, in milliseconds
+					easing: 'ease-in-out', // CSS transition easing function
+
+					// The "opener" function should return the element from which popup will be zoomed in
+					// and to which popup will be scaled down
+					opener: function(openerElement) {
+						// openerElement is the element on which popup was initialized, in this case its <a> tag
+						// you don't need to add "opener" option if this code matches your needs, it's defailt one.
+						return openerElement.is('.img-wrapper') ? openerElement : openerElement.find('.img-wrapper');
+					}
+				}
+			});
+		}, 0);
 	});
 });

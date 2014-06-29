15ed20c789cbe64aee982f5cd2a2cdd4a40c8ba4
freezy <freezy@xbmc.org>
2014-06-30 00:25:56 +0200

    Added model for game.

---
+++ server/models/game.js
@@ -0,0 +1,49 @@
+var _ = require('underscore');
+var logger = require('winston');
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+var Schema = mongoose.Schema;
+
+var gameTypes = [ 'ss', 'em', 'pm', 'og'];
+
+// schema
+var fields = {
+	gameId:        { type: String, required: true, index: true, unique: true },
+	title:         { type: String, required: 'Title must be provided.', index: true },
+	year:          { type: Number, required: 'Year must be provided.', index: true },
+	manufacturer:  { type: String, required: 'Manufacturer must be provided.', index: true },
+	gameType:      { type: String, required: true, enum: { values: gameTypes, message: 'Invalid game type. Valid game types are: ["' +  gameTypes.join('", "') + '"].' }},
+	description:   String,
+	instructions:  String,
+	producedUnits: Number,
+	modelNumber:   String,
+	gameDesign:    Array,
+	artDesign:     Array,
+	ipdb: {
+		number: Number,
+		rating: Number,
+		votes: Number
+	}
+};
+var GameSchema = new Schema(fields);
+
+GameSchema.plugin(uniqueValidator, { message: 'The {PATH} "{VALUE}" is already taken.' });
+
+
+// methods
+GameSchema.methods = {
+
+	/**
+	 * Returns the URL of the file.
+	 *
+	 * @return {String}
+	 * @api public
+	 */
+	getUrl: function() {
+		return '/game/' + this.gameId;
+	}
+};
+
+mongoose.model('Game', GameSchema);
+logger.info('[model] Model "game" registered.');

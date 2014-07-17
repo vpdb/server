bf41a563eea46c5ca30cb86a8a3762ca6c621e29
freezy <freezy@xbmc.org>
2014-07-17 22:07:32 +0200

    Added markup and styling for uploaded files.

--- client/code/controller/release.js
+++ client/code/controller/release.js
@@ -4,7 +4,21 @@ ctrl.controller('ReleaseAddCtrl', function($scope, $upload, $modal, $window, Api
 	$scope.theme('light');
 	$scope.setMenu('admin');
 
+
+	$scope.files = [
+		{
+			name: 'Monster Bash(Williams) (1998) (uw randr) (2.6) (FS).vpt',
+			bytes: 60301235,
+			icon: 'vpt'
+		},
+		{
+			name: 'needed_script.vbs',
+			bytes: 11354,
+			icon: 'code'
+		}
+	];
+
 	$scope.onFilesUpload = function($files) {
 		alert($files);
-	}
+	};
 });

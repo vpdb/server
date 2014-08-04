61af11251d4e6bd8738418780c0efd84f04e59f2
freezy <freezy@xbmc.org>
2014-08-05 00:25:53 +0200

    Added game test.

--- test/api/game.test.js
+++ test/api/game.test.js
@@ -57,6 +57,89 @@ describe('The VPDB `game` API', function() {
 				});
 		});
 
+		it('should fail if the game is already in the database', function(done) {
+
+			var game;
+			async.series([
+
+				// 1. upload backglass 1
+				function(next) {
+					var data = fs.readFileSync(backglass);
+					var fileType = 'backglass';
+					var mimeType = 'image/png';
+					var name = 'backglass.png';
+					request
+						.post('/api/files')
+						.query({ type: fileType })
+						.type(mimeType)
+						.set('Content-Disposition', 'attachment; filename="' + name + '"')
+						.set('Content-Length', data.length)
+						.send(data)
+						.as('contributor')
+						.end(function(res) {
+							expect(res.status).to.be(201);
+							game = hlp.game.getGame({ _media: { backglass: res.body.id }});
+							next();
+						});
+				},
+
+				// 1. post game 1
+				function(next) {
+					request
+						.post('/api/games')
+						.as('contributor')
+						.send(game)
+						.end(function(err, res) {
+							expect(err).to.eql(null);
+							expect(res.status).to.be(201);
+							hlp.doomGame('contributor', res.body.id);
+							next();
+						});
+				},
+
+				// 1. upload backglass 2
+				function(next) {
+					var data = fs.readFileSync(backglass);
+					var fileType = 'backglass';
+					var mimeType = 'image/png';
+					var name = 'backglass.png';
+					request
+						.post('/api/files')
+						.query({ type: fileType })
+						.type(mimeType)
+						.set('Content-Disposition', 'attachment; filename="' + name + '"')
+						.set('Content-Length', data.length)
+						.send(data)
+						.as('contributor')
+						.end(function(res) {
+							expect(res.status).to.be(201);
+							hlp.doomFile('contributor', res.body.id);
+							game._media.backglass = res.body.id;
+							game.id = game.id + '-2';
+							next();
+						});
+				},
+
+				// 1. re-post game 1
+				function(next) {
+					request
+						.post('/api/games')
+						.as('contributor')
+						.send(game)
+						.end(function(err, res) {
+							expect(err).to.eql(null);
+							console.log(res.body);
+							expect(res.status).to.be(422);
+							expect(res.body.errors).to.have.length(1);
+							expect(res.body.errors[0].field).to.be('ipdb.number');
+							expect(res.body.errors[0].message).to.contain('cannot be added twice');
+							next();
+						});
+				}
+			], done);
+
+		});
+
 		it('should fail if a referenced file is not of the same owner');
 		it('should fail if a referenced file is already referenced');
 

a6c98d8278fb415d97ae832e1340868df381a6ca
freezy <freezy@xbmc.org>
2014-10-16 20:47:34 +0200

    Re-enabled previously failing dep.

--- server/grunt-tasks/kss.js
+++ server/grunt-tasks/kss.js
@@ -202,16 +202,12 @@ function toJade(html, done) {
 	if (!html) {
 		return done(null, html);
 	}
-	// TODO re-enable when those are fixed:
-	// 		https://github.com/chad3814/CSSStyleDeclaration/issues/20
-	//		https://github.com/chad3814/CSSStyleDeclaration/issues/19
-	return done(null, html);
-//	html2jade.convertHtml(html, {}, function(err, jade) {
-//		if (err) {
-//			return done(err);
-//		}
-//		done(null, jade);
-//	});
+	html2jade.convertHtml(html, {}, function(err, jade) {
+		if (err) {
+			return done(err);
+		}
+		done(null, jade);
+	});
 }
 
 function sectionSort(a, b) {

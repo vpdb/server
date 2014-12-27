15e084e311d312b98a057f83a428e9c2a95c0911
freezy <freezy@xbmc.org>
2014-12-27 13:49:46 +0100

    Added (local) password change to UI.

--- client/app/profile/settings.js
+++ client/app/profile/settings.js
@@ -10,25 +10,16 @@ angular.module('vpdb.profile.settings', [])
 
 		var user = AuthService.getUser();
 
-		// user profile that will be sent for update
-		$scope.updatedUser = _.pick(user, 'name', 'location', 'email');
-
-		// local user for changing password
-		$scope.localUser = {};
+		$scope.updatedUser = _.pick(user, 'name', 'location', 'email'); // user profile that will be sent for update
+		$scope.localUser = {};                                          // local user for changing password
+		$scope.localCredentials = {};                                   // local credentials object
 
 		$scope.providers = AuthService.getProviders(user);
 		var allProviders = AuthService.getProviders();
 
-		// pre-fill (local) username from first provider we find.
-		var i, provider;
-		for (i = 0; i < allProviders.length; i++) {
-			provider = allProviders[i];
-			if (user[provider.id] && user[provider.id].username) {
-				$scope.localUser.username = user[provider.id].username;
-				break;
-			}
-		}
-
+		/**
+		 * First block: Update "public" user profile data
+		 */
 		$scope.updateUserProfile = function() {
 			ProfileResource.patch($scope.updatedUser, function(user) {
 				AuthService.saveUser(user);
@@ -38,33 +29,91 @@ angular.module('vpdb.profile.settings', [])
 			}, ApiHelper.handleErrors($scope));
 		};
 
+
+		/**
+		 * CHANGE PASSWORD
+		 * Updates local password.
+		 */
+		$scope.changePassword = function() {
+
+			ApiHelper.clearErrors($scope);
+
+			// password match is checked locally, no such test on server side.
+			if (!checkPasswordConfirmation($scope.localUser)) {
+				return;
+			}
+
+			// update server side
+			ProfileResource.patch({
+				current_password: $scope.localUser.currentPassword,
+				password: $scope.localUser.password1
+			}, function() {
+				$scope.localUser = {};
+				ApiHelper.clearErrors($scope);
+				$scope.showNotification('Password successfully changed.', 3000);
+
+			}, ApiHelper.handleErrors($scope));
+		};
+
+
+		/**
+		 * LOCAL CREDENTIALS
+		 * Create new local credentials if the user was only registered via
+		 * OAuth2.
+		 */
 		$scope.createLocalCredentials = function() {
 
 			ApiHelper.clearErrors($scope);
 
 			// password match is checked locally, no such test on server side.
-			if (!$scope.localUser.password1) {
-				return ApiHelper.setError($scope, 'password', "You must provide your new password.");
-			}
-			if (!$scope.localUser.password2) {
-				return ApiHelper.setError($scope, 'password2', "You must confirm your new password.");
-			}
-			if ($scope.localUser.password1 !== $scope.localUser.password2) {
-				return ApiHelper.setError($scope, 'password2', "The second password must match the first password.");
+			if (!checkPasswordConfirmation($scope.localCredentials)) {
+				return;
 			}
 
-			// now patch user on server side
+			// update server side
 			ProfileResource.patch({
-				username: $scope.localUser.username,
-				password: $scope.localUser.password1
+				username: $scope.localCredentials.username,
+				password: $scope.localCredentials.password1
 			}, function(user) {
 				AuthService.saveUser(user);
 				ApiHelper.clearErrors($scope);
-
-				$scope.showNotification('Local credentials successfully created. You may login with username <strong>' + $scope.localUser.username + '</strong> now.', 5000);
+				$scope.showNotification('Local credentials successfully created. You may login with username <strong>' + $scope.localCredentials.username + '</strong> now.', 5000);
 
 			}, ApiHelper.handleErrors($scope));
+		};
 
+		// pre-fill (local) username from first provider we find.
+		var i, provider;
+		for (i = 0; i < allProviders.length; i++) {
+			provider = allProviders[i];
+			if (user[provider.id] && user[provider.id].username) {
+				$scope.localCredentials.username = user[provider.id].username;
+				break;
+			}
+		}
+
+
+		/**
+		 * Checks that two passwords field are filled and equal and applies
+		 * errors to scope.
+		 *
+		 * @param {object} credentials Objection containing `password1` and `password2`.
+		 * @returns {boolean} True on success, false otherwise.
+		 */
+		var checkPasswordConfirmation = function(credentials) {
+			if (!credentials.password1) {
+				ApiHelper.setError($scope, 'password', "You must provide your new password.");
+				return false;
+			}
+			if (!credentials.password2) {
+				ApiHelper.setError($scope, 'password2', "You must confirm your new password.");
+				return false;
+			}
+			if (credentials.password1 !== credentials.password2) {
+				ApiHelper.setError($scope, 'password2', "The second password must match the first password.");
+				return false;
+			}
+			return true;
 		};
 
 	});

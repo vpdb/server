addf35770b8553a0b306f118154a96de5522f718
freezy <freezy@xbmc.org>
2015-01-04 14:00:55 +0100

    Added collapsable info panel.

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -22,40 +22,43 @@ angular.module('vpdb.home', [])
 			$rootScope.login();
 		}
 
-		$http({
-			method: 'GET',
-			url: '/api-mock/packs'
-		}).success(function(data, status, headers, config) {
-			$scope.packs = data.result;
-		});
+		$scope.whatsThis = true;
 
-		$http({
-			method: 'GET',
-			url: '/api-mock/releases?show=new'
-		}).success(function(data, status, headers, config) {
-			$scope.newReleases = data.result;
-		});
 
-		$http({
-			method: 'GET',
-			url: '/api-mock/releases?show=updated'
-		}).success(function(data, status, headers, config) {
-			$scope.updatedReleases = data.result;
-		});
-
-		$http({
-			method: 'GET',
-			url: '/api-mock/feed'
-		}).success(function(data, status, headers, config) {
-			$scope.feed = data.result;
-		});
-
-		$http({
-			method: 'GET',
-			url: '/api-mock/users'
-		}).success(function(data, status, headers, config) {
-			$scope.users = data.result;
-		});
+		//$http({
+		//	method: 'GET',
+		//	url: '/api-mock/packs'
+		//}).success(function(data, status, headers, config) {
+		//	$scope.packs = data.result;
+		//});
+		//
+		//$http({
+		//	method: 'GET',
+		//	url: '/api-mock/releases?show=new'
+		//}).success(function(data, status, headers, config) {
+		//	$scope.newReleases = data.result;
+		//});
+		//
+		//$http({
+		//	method: 'GET',
+		//	url: '/api-mock/releases?show=updated'
+		//}).success(function(data, status, headers, config) {
+		//	$scope.updatedReleases = data.result;
+		//});
+		//
+		//$http({
+		//	method: 'GET',
+		//	url: '/api-mock/feed'
+		//}).success(function(data, status, headers, config) {
+		//	$scope.feed = data.result;
+		//});
+		//
+		//$http({
+		//	method: 'GET',
+		//	url: '/api-mock/users'
+		//}).success(function(data, status, headers, config) {
+		//	$scope.users = data.result;
+		//});
 
 	})
 

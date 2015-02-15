0b237789abe6cc545862de28e01085e9ce6a4237
freezy <freezy@xbmc.org>
2015-02-16 00:43:38 +0100

    Added some initial ROM data.

---
+++ data/gen/roms/roms.js
@@ -0,0 +1,179 @@
+/*
+ * VPDB - Visual Pinball Database
+ * Copyright (C) 2015 freezy <freezy@xbmc.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+exports.data = {
+	afm: {
+		afm_10: {version: '1.0'},
+		afm_11: {version: '1.1'},
+		afm_113: {version: '1.13'},
+		afm_113b: {version: '1.13b'},
+		afm_11u: {version: '1.1', notes: 'Ultrapin'},
+		afm_f10: {version: '0.10', notes: 'FreeWPC'},
+		afm_f20: {version: '0.20', notes: 'FreeWPC'},
+		afm_f32: {version: '0.32', notes: 'FreeWPC'}
+	},
+	bttf: {
+		bttf_a20: {version: '2.0'},
+		bttf_a21: {version: '2.1'},
+		bttf_a27: {version: '2.7'},
+		bttf_g27: {version: '2.7', language: 'de'}
+	},
+	bbb: {
+		bbb108: {version: '1.08'},
+		bbb109: {version: '1.09'}
+	},
+	bk2k: {
+		bk2k_l4: {version: 'L-4'},
+		bk2k_la2: {version: 'LA-2'},
+		bk2k_lg1: {version: 'L-1', language: 'de'},
+		bk2k_lg3: {version: 'L-3', language: 'de'},
+		bk2k_pa7: {version: 'PA-7'},
+		bk2k_pu1: {version: 'PU-1'}
+	},
+	br: {
+		br_l1: {version: 'L-1'},
+		br_l3: {version: 'L-3'},
+		br_l4: {version: 'L-4'},
+		br_p17: {version: 'SP-1'}
+	},
+	bsd: {
+		drac_l1: {version: 'L-1'},
+		drac_p11: {version: 'P-11'}
+	},
+	cc: {
+		cc_10: {version: '1.0'},
+		cc_104: {version: '1.04', notes: 'Test 0.2'},
+		cc_12: {version: '1.2'},
+		cc_13: {version: '1.3'},
+		cc_13k: {version: '1.3k', notes: 'Adds support for a real knocker'}
+	},
+	centaur: {
+		centaur: {},
+		centaura: {version: '1', notes: 'Free play'},
+		centaurb: {version: '27', notes: 'Free play'}
+	},
+	cv: {
+		cv_10: {version: '1.0'},
+		cv_11: {version: '1.1'},
+		cv_13: {version: '1.3'},
+		cv_14: {version: '1.4'},
+		cv_20h: {version: '2.0h'}
+	},
+	congo: {
+		congo_11: {version: '1.1'},
+		congo_13: {version: '1.3'},
+		congo_20: {version: '2.0'},
+		congo_21: {version: '2.1'}
+	},
+	cftbl: {
+		cftbl_l3: {version: 'L-3'},
+		cftbl_l4: {version: 'L-4'},
+		cftbl_p3: {version: 'P-3'}
+	},
+	cyclone: {
+		cycln_l1: {version: 'L-1'},
+		cycln_l4: {version: 'L-4'},
+		cycln_l5: {version: 'L-5'}
+	},
+	dm: {
+		dm_dt099: {version: '0.99', notes: 'FreeWPC Demolition Time by James Cardona'},
+		dm_h5: {version: 'H-5'},
+		dm_h5b: {version: 'H-5', notes: 'Coin Play'},
+		dm_h6: {version: 'H-6'},
+		dm_h6b: {version: 'H-6', notes: 'Coin Play'},
+		dm_la1: {version: 'L-1'},
+		dm_lx3: {version: 'L-3'},
+		dm_lx4: {version: 'L-4'},
+		dm_pa2: {version: 'P-2'},
+		dm_px5: {version: 'P-5'}
+	},
+	diner: {
+		diner_l1: {version: 'L-1'},
+		diner_l3: {version: 'L-3'},
+		diner_l4: {version: 'L-4'}
+	},
+	dd: {
+		dd_l2: {version: 'L-2'},
+		dd_p06: {version: 'PA-6', notes: 'WPC'},
+		dd_p6: {version: 'P-6'},
+		dd_p7: {version: 'PA-7', notes: 'WPC'}
+	},
+	dw: {
+		dw_l1: {version: 'L-1'},
+		dw_l2: {version: 'L-2'},
+		dw_p5: {version: 'P-5'}
+	},
+	es: {
+		esha_la1: {version: 'L-1'},
+		esha_la3: {version: 'L-3'},
+		esha_lg1: {version: 'L-1', language: 'de'},
+		esha_lg2: {version: 'L-2', language: 'de'},
+		esha_ma3: {version: 'M-3', notes: 'Metallica'},
+		esha_pa1: {version: 'P-1'},
+		esha_pr4: {version: 'PR-4', notes: 'Family ROM'}
+	},
+	eatpm: {
+		eatpm_4g: {version: 'L-4', language: 'de'},
+		eatpm_4u: {version: 'L-4U'},
+		eatpm_l1: {version: 'L-1'},
+		eatpm_l2: {version: 'L-2'},
+		eatpm_l4: {version: 'L-1'},
+		eatpm_p7: {version: 'P-7'}
+	},
+	firepower: {
+		frpwr_a6: {version: '31', notes: 'System 6 - 6-digit custom'},
+		frpwr_a7: {version: '31', notes: 'System 7 - 6-digit custom'},
+		frpwr_b6: {version: '31', notes: 'System 6 - 7-digit'},
+		frpwr_b7: {version: '31', notes: 'System 7 - 7-digit'},
+		frpwr_c6: {version: '31', notes: 'System 6 - 7-digit custom'},
+		frpwr_c7: {version: '38', notes: 'System 7 - 7-digit custom'},
+		frpwr_d6: {version: '31', notes: 'System 6 - 6-digit/10 scoring'},
+		frpwr_d7: {version: '31', notes: 'System 7 - 7-digit custom'},
+		frpwr_e7: {version: '31', notes: 'System 7 - 6-digit/10 scoring'},
+		frpwr_l2: {version: 'L-2'},
+		frpwr_l6: {version: 'L-6'},
+		frpwr_t6: {version: 'T-6'}
+	},
+	ft: {
+		ft_l3: {version: 'L-3'},
+		ft_l4: {version: 'L-4'},
+		ft_l5: {version: 'L-5'},
+		ft_p4: {version: 'P-4'}
+	},
+	fh: {
+		fh_l2: {version: 'L-2'},
+		fh_l3: {version: 'L-3'},
+		fh_l4: {version: 'L-4'},
+		fh_l5: {version: 'L-5'},
+		fh_l9: {version: 'L-9'},
+		fh_l9b: {version: 'L-9', notes: 'Bootleg'},
+		fh_905h: {version: 'L-905H'},
+		fh_906h: {version: 'L-906H', notes: 'Coin Play'}
+	},
+	ij: {
+		ij_h1: {version: 'HK-1', notes: 'Family ROM'},
+		ij_l3: {version: 'L-3'},
+		ij_l4: {version: 'L-4'},
+		ij_l5: {version: 'L-5'},
+		ij_l6: {version: 'L-6'},
+		ij_l7: {version: 'L-7'},
+		ij_lg7: {version: 'L-7', language: 'de'},
+		ij_p2: {version: 'P-2'}
+	}
+};

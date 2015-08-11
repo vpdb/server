9f8d97ac073f7fb513a09677503589085e8ef01b
freezy <freezy@xbmc.org>
2015-08-11 17:42:01 +0200

    Enabled Travis' Docker container.

--- deploy/ci/travis.sh
+++ deploy/ci/travis.sh
@@ -3,18 +3,6 @@
 # Stop on error
 set -e
 
-# Install dependencies
-echo travis_fold:start:Dependencies
-sudo apt-get update
-sudo apt-get -y install graphicsmagick pngquant optipng
-sudo wget http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz
-sudo mkdir -p bin
-cd bin
-sudo tar xvf ../ffmpeg-release-64bit-static.tar.xz --strip 1 --no-anchored ffmpeg ffprobe
-cd ..
-export PATH=$(pwd)/bin:$PATH
-echo travis_fold:end:Dependencies
-
 # Print versions
 echo travis_fold:start:Versions
 echo "node version: $(node --version)"

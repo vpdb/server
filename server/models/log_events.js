161d133c5820db7b9f2e666fae2b8a6194121cde
freezy <freezy@xbmc.org>
2015-08-17 23:23:18 +0200

    Added model for event logging.

---
+++ server/models/log_events.js
@@ -0,0 +1,87 @@
+/*
+ * VPDB - Visual Pinball Database
+ * Copyright (C) 2015 freezy <freezy@xbmc.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+"use strict";
+
+var _ = require('lodash');
+var logger = require('winston');
+var mongoose = require('mongoose');
+var paginate = require('mongoose-paginate');
+var toObj = require('./plugins/to-object');
+
+var Schema = mongoose.Schema;
+
+//-----------------------------------------------------------------------------
+// SCHEMA
+//-----------------------------------------------------------------------------
+var fields = {
+	_user:       { type: Schema.ObjectId, required: true, ref: 'User', index: true },
+	_ref: {
+		game:    { type: Schema.ObjectId, ref: 'Game', index: true, sparse: true },
+		release: { type: Schema.ObjectId, ref: 'Release', index: true, sparse: true },
+		user:    { type: Schema.ObjectId, ref: 'User', index: true, sparse: true }
+	},
+	type:        { type: String, 'enum': [ 'game', 'release', 'user' ], required: true, index: true },
+	data:        { },
+	logged_at:   { type: Date, required: true }
+};
+var LogEventSchema = new Schema(fields);
+
+
+//-----------------------------------------------------------------------------
+// PLUGINS
+//-----------------------------------------------------------------------------
+LogEventSchema.plugin(paginate);
+LogEventSchema.plugin(toObj);
+
+
+//-----------------------------------------------------------------------------
+// VIRTUALS
+//-----------------------------------------------------------------------------
+LogEventSchema.virtual('user')
+	.get(function() {
+		if (this.populated('_user') && this._user) {
+			return this._user.toReduced();
+		}
+		return undefined;
+	});
+
+
+//-----------------------------------------------------------------------------
+// STATIC METHODS
+//-----------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------
+// OPTIONS
+//-----------------------------------------------------------------------------
+LogEventSchema.options.toObject = {
+	virtuals: true,
+	transform: function(doc, log) {
+		delete log.__v;
+		delete log._id;
+		delete log._user;
+		delete log._ref;
+		delete log.id;
+	}
+};
+
+
+mongoose.model('LogEventSchema', LogEventSchema);
+logger.info('[model] Schema "LogEventSchema" registered.');

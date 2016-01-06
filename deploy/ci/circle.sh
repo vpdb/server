9d9479737eb4261715017efde77d56d831a0db32
freezy <freezy@xbmc.org>
2016-01-06 13:07:07 +0100

    Fixed paths in CircleCI config.

--- deploy/ci/circle.sh
+++ deploy/ci/circle.sh
@@ -2,5 +2,6 @@
 
 echo "node version: $(node --version)"
 echo "npm version: $(npm --version)"
+echo "which ffmpeg: $(which ffmpeg)"
 echo "ffmpeg version: $(ffmpeg -version)"
 echo "pngquant version: $(pngquant --version)"

6489605a69d4a53e51d1c740e39c45663b86040b
freezy <freezy@xbmc.org>
2016-01-06 12:52:35 +0100

    Updated CircleCI config.

---
+++ deploy/ci/circle.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "node version: $(node --version)"
+echo "npm version: $(npm --version)"
+echo "ffmpeg version: $(ffmpeg -version)"
+echo "pngquant version: $(pngquant --version)"
--- deploy/ci/codeship.sh
+++ deploy/ci/codeship.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # This is just for reference what you'd configure at Codeship.
-nvm install 0.10.25
-nvm use 0.10.25
+nvm install 5.3.0
+nvm use 5.3.0
 npm install
 
 # deps
@@ -11,11 +11,15 @@ mkdir -p bin
 cd bin
 tar -xvf ../ffmpeg-release-64bit-static.tar.xz --strip 1 --no-anchored ffmpeg ffprobe
 cd ..
+wget http://www.rarlab.com/rar/rarlinux-x64-5.3.0.tar.gz
+tar -xvf rarlinux-x64-5.3.0.tar.gz
+mv rar/* bin
+
 export PATH=$(pwd)/bin:$PATH
 echo "node version: $(node --version)"
 echo "npm version: $(npm --version)"
 echo "ffmpeg version: $(ffmpeg -version)"
 
 # Install grunt-cli for running your tests or other tasks
-npm install grunt-cli
+npm install grunt-cli codeclimate-test-reporter
 grunt git

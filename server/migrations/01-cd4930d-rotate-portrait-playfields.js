5a60c7218436ffdf6f3eaff52057709468623ac0
freezy <freezy@xbmc.org>
2016-02-13 13:26:17 +0100

    Added migration scripts for playfield changes.

---
+++ server/migrations/01-cd4930d-rotate-portrait-playfields.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const gm = require('gm');
+const fs = require('fs');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User');
+const File = mongoose.model('File');
+const processor = require('../modules/processor/image');
+
+module.exports.up = function() {
+
+	return Promise.each(File.find({ file_type: 'playfield-fs' }).exec(), file => {
+
+		if (!/^image\//i.test(file.mime_type)) {
+			return;
+		}
+
+		if (file.metadata.size.width > file.metadata.size.height) {
+
+			let original = file.getPath();
+			let dest = file.getPath(null, '_reprocessing');
+
+			let img = gm(original);
+			img.rotate('black', -90);
+
+			return new Promise((resolve, reject) => {
+				let writeStream = fs.createWriteStream(dest);
+
+				// setup success handler
+				writeStream.on('finish', function() {
+					console.log(file.name);
+					resolve();
+				});
+				writeStream.on('error', reject);
+				img.stream().on('error', reject).pipe(writeStream).on('error', reject);
+
+			}).then(() => {
+				if (fs.existsSync(original)) {
+					fs.unlinkSync(original);
+				}
+				fs.renameSync(dest, original);
+
+				return processor.metadata(file);
+
+			}).then(metadata => {
+				File.sanitizeObject(metadata);
+				file.metadata = metadata;
+				return file.save();
+
+			}).catch(console.error);
+		}
+	});
+};
---
+++ server/migrations/02-278f5ff-reprocess-playfield-variations.js
@@ -0,0 +1,88 @@
+"use strict";
+
+const gm = require('gm');
+const fs = require('fs');
+const rimraf = Promise.promisify(require('rimraf'));
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User');
+const File = mongoose.model('File');
+
+const storage = require('../modules/storage');
+const config = require('../modules/settings').current;
+
+// bootstrap processors
+const processors = {
+	image: require('../modules/processor/image'),
+	video: require('../modules/processor/video'),
+	table: require('../modules/processor/table'),
+	archive: require('../modules/processor/archive')
+};
+const variations = {
+	image: processors.image.variations,
+	video: processors.video.variations,
+	table: processors.table.variations
+};
+
+module.exports.up = function() {
+
+	let query = { $or: [
+		{ file_type: 'playfield-fs' },
+		{ file_type: 'playfield-ws' }
+	] };
+	return Promise.each(File.find(query).exec(), file => {
+
+		const mimeCategory = file.getMimeCategory();
+		const processor = processors[mimeCategory];
+
+		if (!processor) {
+			return;
+		}
+
+		console.log('Processing %s %s %s - %s...', file.file_type, processor.name, file.id, file.name);
+
+		// update metadata
+		return processor.metadata(file).then(metadata => {
+			File.sanitizeObject(metadata);
+			file.metadata = metadata;
+			file.variations = {};
+			return file.save();
+
+		}).then(() => {
+
+			// process pass 1
+			if (variations[mimeCategory] && variations[mimeCategory][file.file_type]) {
+
+				return Promise.each(variations[mimeCategory][file.file_type], variation => {
+					let original = file.getPath(variation);
+					let dest = file.getPath(variation, '_reprocessing');
+					console.log('   -> %s: %s', variation.name, dest);
+					return processor.pass1(file.getPath(), dest, file, variation).then(() => {
+							if (fs.existsSync(original)) {
+								fs.unlinkSync(original);
+							}
+							fs.renameSync(dest, original);
+						})
+						.then(() => storage.onProcessed(file, variation, processor))
+						.catch(err => console.error("ERROR: %s", err.message));
+				});
+			}
+		});
+	}).then(() => {
+
+		// delete folders "medium-landscape" and "medium-landscape-2x"
+		return Promise.each([
+			config.vpdb.storage.public.path + '/medium-landscape',
+			config.vpdb.storage.public.path + '/medium-landscape-2x',
+			config.vpdb.storage.protected.path + '/medium-landscape',
+			config.vpdb.storage.protected.path + '/medium-landscape-2x'
+		], folder => {
+			if (fs.existsSync(folder)) {
+				console.log('Deleting folder "%s"...', folder);
+				return rimraf(folder);
+			} else {
+				console.log('Skipping folder "%s"...', folder);
+			}
+		})
+	});
+};

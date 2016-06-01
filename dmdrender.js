08c5fed6bfad393b1a573c98cdc1c0856595f24f
freezy <freezy@kodi.tv>
2016-06-02 01:16:56 +0200

    Fixed DMD renderer with direct pipe instead of fluent-ffmpeg.

--- dmdrender.js
+++ dmdrender.js
@@ -19,97 +19,125 @@
 
 "use strict";
 
+const _ = require('lodash');
 const fs = require('fs');
-const gm = require('gm');
-const PNG = require('pngjs').PNG;
 const ffmpeg = require('fluent-ffmpeg');
-const PassThrough = require('stream').PassThrough;
-const Promise = require('bluebird');
+const Readable = require('stream').Readable;
+const util = require('util');
 
-const width = 128;
-const height = 32;
-const shades = 16;
+/**
+ * Reads the DMD capture text files and converts them into frame buffers.
+ *
+ * @param {String} filename Path to capture text file
+ * @param {{ width: Number, height: Number, shades: Number }} opts Options
+ * @constructor
+ */
+function DmdFrameReader(filename, opts) {
+	this._started = new Date().getTime();
+	let dump = fs.readFileSync(filename).toString('utf8');
+	this._frames = dump.split('\r\n\r\n');
+	this._hsl = rgbToHsl(0xff, 0x6a, 0x00);
+	this._n = 0;
 
-let dump = fs.readFileSync('avg_170_dump.txt').toString('utf8');
-let frames = dump.split('\r\n\r\n');
-console.log('Read %s frames.', frames.length);
-let hsl = rgbToHsl(0xff, 0x6a, 0x00);
-let n = 0;
-let jpgStream = new PassThrough();
-ffmpeg()
-	.input(jpgStream)
-	.inputFormat('image2pipe')
-	.inputOptions('-vcodec mjpeg')
-	.fps(30)
-	.format('mp4')
-	.outputOptions('-movflags frag_keyframe+empty_moov')
-	.videoCodec('libx264')
-//	.videoBitrate(1000, true)
-	.output(fs.createWriteStream('frames.mp4'), { end: false })
-	.on('start', function(commandLine) {
-		console.log('Spawned Ffmpeg with command: ' + commandLine);
+	opts = opts || {};
+	this.width = opts.width || 128;
+	this.height = opts.height || 32;
+	this.shades = opts.shades || 16;
 
-		Promise.each(frames, frameData => {
-			return new Promise((resolve, reject) => {
-				let lines = frameData.split('\r\n');
-				let frame = new PNG({ width: width, height: height, bgColor: { red: 0, green: 0, blue: 0 } });
-				console.log('%s %s', n, lines[0]);
-				if (!frameData) {
-					return resolve();
-				}
-				for (let y = 0; y < height; y++) {
-					for (let x = 0; x < width; x++) {
-						try {
-							let opacity = (parseInt(lines[y + 1][x], 16) + 1) / shades;
-							let idx = (width * y + x) << 2;
-							let rgb = hslToRgb(hsl[0], hsl[1], opacity * hsl[2]);
-							frame.data[idx] = rgb[0];
-							frame.data[idx + 1] = rgb[1];
-							frame.data[idx + 2] = rgb[2];
-							frame.data[idx + 3] = 255;
+	console.log('Read %s frames.', this._frames.length);
+}
 
-						} catch (err) {
-							console.error('Error parsing DMD data: %s', err.message);
-							console.log(frameData);
-							return resolve();
-						}
-					}
-				}
-				frame.on('error', reject);
-				frame.on('end', resolve);
-				//frame.on('end', () => { setTimeout(resolve, 20); });
-				//frame.pack().pipe(fs.createWriteStream('frame_' + n + '.png'));
-				//gm(frame.pack()).setFormat('jpg').quality(100).stream().pipe(fs.createWriteStream('frame_' + n + '.jpg'));
-				gm(frame.pack()).setFormat('jpg').quality(100).stream().pipe(jpgStream, { end: false });
-				//gm(frame.pack()).setFormat('jpg').quality(100).stream().on('data', data => { jpgStream.push(data); });
-				//frame.pack().on('data', data => { pngStream.push(data); });
-				n++;
-			});
-		}).then(() => {
-			//jpgStream.push(null);
-			jpgStream.emit('end');
-			jpgStream.emit('finish');
-			console.log('%s Frames sent to ffmpeg.', n);
-		});
-	})
-	.on('codecData', data => {
-		console.log('CODEC DATA:');
-		console.log(data);
-	})
-	.on('progress', progress => {
-		console.log('PROGRESS:');
-		console.log(progress);
-	})
-	.on('stderr', stderr => {
-		console.log(stderr);
-	})
-	.on('error', err => {
-		console.log('Error rendering: %s', err.message);
-	})
-	.on('end', () => {
-		console.log('Video saved!');
-	})
-	.run();
+/**
+ * Copies the next frame to the frame buffer.
+ *
+ * @param {Buffer} buffer Frame buffer to write to
+ * @returns {boolean} If true, there are still frames to read.
+ */
+DmdFrameReader.prototype.read = function(buffer) {
+
+	if (this._n >= this._frames.length) {
+		console.log('Done in %sms.', new Date().getTime() - this._started);
+		return false;
+	}
+	const frameData = this._frames[this._n];
+	if (!frameData) {
+		this._n++;
+		return true;
+	}
+	const lines = frameData.split('\r\n');
+	for (let y = 0; y < this.height; y++) {
+		for (let x = 0; x < this.width; x++) {
+			try {
+				let opacity = (parseInt(lines[y + 1][x], 16) + 1) / this.shades;
+				let idx = (this.width * y + x) * 3;
+				let rgb = hslToRgb(this._hsl[0], this._hsl[1], opacity * this._hsl[2]);
+				buffer[idx] = rgb[0];
+				buffer[idx + 1] = rgb[1];
+				buffer[idx + 2] = rgb[2];
+
+			} catch (err) {
+				console.error('Error parsing DMD data: %s', err.message);
+				console.error(err.stack);
+				console.log(frameData);
+				return false;
+			}
+		}
+	}
+	this._n++;
+	return true;
+};
+
+/**
+ * A readable stream that returns RGB24 frames.
+ *
+ * @param generator Generator class that produces frames
+ * @param {{ width: Number, height: Number }} opts Options
+ * @constructor
+ */
+function RawImageStream(generator, opts) {
+	// init Readable
+	Readable.call(this, opts);
+	opts = opts || {};
+
+	if (!_.isFunction(generator.read)) {
+		throw new Error('Generator must contain a read() function!');
+	}
+
+	this._generator = generator;
+	this.width = opts.width || 128;
+	this.height = opts.height || 32;
+}
+util.inherits(RawImageStream, Readable);
+RawImageStream.prototype._read = function() {
+	let rawImage = new Buffer(this.width * this.height * 3);
+	if (this._generator.read(rawImage)) {
+		this.push(rawImage);
+		return true;
+	}
+	console.error('done!');
+	this.push(null);
+	return false;
+};
+
+const opts = {
+	width: 128,
+	height: 32,
+	shades: 16
+};
+
+//  ffmpeg -y -f rawvideo -s 128x32 -pix_fmt rgb24 -i - -vcodec libx264 -f mp4 frames.mp4
+
+const dmdRawFrameStream = new RawImageStream(new DmdFrameReader('avg_170_dump.txt', opts), opts);
+const spawn = require('child_process').spawn;
+const ps = spawn('ffmpeg', [
+	'-y', '-f', 'rawvideo', '-s', '128x32', '-pix_fmt', 'rgb24', '-i', '-',
+	'-vcodec', 'libx264', '-f', 'mp4', '-b', '100k', 'frames.mp4']);
+
+ps.stdout.on('data', data => console.log(data.toString()));
+ps.stderr.on('data', data => console.log(data.toString()));
+ps.on('close', code => console.log(`done! (${code})`));
+dmdRawFrameStream.pipe(ps.stdin);
+
 
 /**
  * Converts an HSL color value to RGB. Conversion formula
@@ -181,6 +209,5 @@ function rgbToHsl(r, g, b) {
 		}
 		h /= 6;
 	}
-
 	return [h, s, l];
 }

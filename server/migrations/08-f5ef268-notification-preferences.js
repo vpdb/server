7641b88d94dff6fadc3a303d83af623f347225aa
freezy <freezy@kodi.tv>
2016-09-17 01:09:19 +0200

    Added migration script for new game request notification setting.

---
+++ server/migrations/08-f5ef268-notification-preferences.js
@@ -0,0 +1,35 @@
+/*
+ * VPDB - Visual Pinball Database
+ * Copyright (C) 2016 freezy <freezy@xbmc.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+"use strict";
+
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+module.exports.up = function(grunt) {
+	return User.find().exec().then(users => {
+		grunt.log.write('Setting game requests notification flag for %s users.', users.length);
+		return Promise.each(users, user => {
+			if (user.preferences) {
+				user.preferences.notify_game_requests = true;
+				return user.save();
+			}
+		});
+	});
+};

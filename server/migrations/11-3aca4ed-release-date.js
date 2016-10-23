7ea278d91551033f217449188f2b4adc3dec561a
freezy <freezy@kodi.tv>
2016-10-24 00:10:36 +0200

    Added migration script for new release field.

---
+++ server/migrations/11-3aca4ed-release-date.js
@@ -0,0 +1,38 @@
+/*
+ * VPDB - Visual Pinball Database
+ * Copyright (C) 2016 freezy <freezy@xbmc.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+"use strict";
+
+const _ = require('lodash');
+const mongoose = require('mongoose');
+const Release = mongoose.model('Release');
+
+module.exports.up = function(grunt) {
+	return Release.find().exec().then(releases => {
+		grunt.log.write('Adding released_at to %s releases...', releases.length);
+		return Promise.each(releases, release => {
+			if (!release.license) {
+				release.license = 'by-sa';
+			}
+			release.versions = _.sortBy(release.versions, ['released_at']);
+			release.released_at = release.versions[release.versions.length - 1].released_at;
+			return release.save();
+		});
+	});
+};

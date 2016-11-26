e847fe42e04fe6212b085674c714a9d408733591
freezy <freezy@kodi.tv>
2016-11-27 00:22:50 +0100

    Fixed crash when requesting media that doesn't exist on download.

--- server/controllers/storage/releases.js
+++ server/controllers/storage/releases.js
@@ -144,10 +144,10 @@ exports.download = function(req, res) {
 						numTables++;
 
 						// add media if checked
-						if (body.playfield_image) {
+						if (body.playfield_image && versionFile._playfield_image) {
 							requestedFiles.push(versionFile._playfield_image);
 						}
-						if (body.playfield_video) {
+						if (body.playfield_video && versionFile._playfield_video) {
 							requestedFiles.push(versionFile._playfield_video);
 						}
 					}

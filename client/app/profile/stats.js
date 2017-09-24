2e5e85393f1b7a12acfda3c8d16044061e09525d
freezy <freezy@kodi.tv>
2017-09-25 00:10:19 +0200

    Removed client files.

--- client/app/profile/stats.js
+++
@@ -1,9 +0,0 @@
-"use strict"; /* global _, angular*/
-
-angular.module('vpdb.profile.stats', [])
-
-	.controller('ProfileStatsCtrl', function($scope, $rootScope, AuthService, ApiHelper, ProfileResource) {
-
-		$scope.logins = ProfileResource.logs({ event: 'authenticate', per_page: 10 });
-
-	});

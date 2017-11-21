6fca5b960c43637962b36ee3095d98f748b9ce7b
freezy <freezy@kodi.tv>
2017-11-21 01:25:46 +0100

    Added BrowserStack configuration.

--- src/config/settings-browserstack.js
+++ src/config/settings-browserstack.js
@@ -10,15 +10,15 @@ module.exports = {
 			'public': { path: './data/storage-test-public', api: { protocol: 'http', hostname: 'localhost', port: 7357, pathname: '/storage/public', prefix: '' } },
 			'protected': { path: './data/storage-test-protected', api: { protocol: 'http', hostname: 'localhost', port: 7357, pathname: '/storage/v1', prefix: '' } }
 		},
-		webapp: { protocol: 'http', hostname: 'localhost', port: 3333 },
-		db: 'mongodb://localhost/vpdb-test',
+		webapp: { protocol: 'http', hostname: 'localhost', port: 7357 },
+		db: 'mongodb://localhost/vpdb-browserstack',
 		redis: { host: '127.0.0.1', port: 6379, db: 7 },
 		apiTokenLifetime: 3600000,
 		storageTokenLifetime: 60000,
 		secret: 'do-not-run-this-config-in-production!',
 		loginBackoff: { delay: [ 0 ], keep: 0 },
 		email: {
-			confirmUserEmail: true,
+			confirmUserEmail: false,
 			sender: { email: 'server@vpdb.local', name: 'VPDB Server' },
 			nodemailer: {}
 		},
@@ -54,10 +54,9 @@ module.exports = {
 		pusher: { enabled: false, options: { } },
 		passport: {
 			google: { enabled: false, clientID: 'TEST_CLIENT_ID', clientSecret: 'TEST_CLIENT_SECRET' },
-			github: { enabled: true, clientID: 'TEST_CLIENT_ID', clientSecret: 'TEST_CLIENT_SECRET' },
+			github: { enabled: false, clientID: 'TEST_CLIENT_ID', clientSecret: 'TEST_CLIENT_SECRET' },
 			ipboard: [{ enabled: true, id: 'ipbtest', name: 'Test', icon: '', baseURL: 'https://vpdb.io/forums/index.php', clientID: 'TEST_CLIENT_ID', clientSecret: 'TEST_CLIENT_SECRET', version: 3 }]
 		}
 	},
 	webapp: { ga: { enabled: false, id: '' } }
-	//, ffmpeg: { path: 'E:\\Tools\\FFmpeg\\bin\\ffmpeg.exe' }
 };

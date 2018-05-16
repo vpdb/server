f6ccd2640586d4950f620a7a00d9e231cd91f9c1
freezy <freezy@kodi.tv>
2018-05-16 20:48:48 +0200

    refactor: Move auth into Koa middleware.

--- src/common/api.ts
+++ src/common/api.ts
@@ -44,14 +44,13 @@ export abstract class Api {
 	}
 
 	/**
-	 * No authorization is done but tokens are still read and user object
-	 * updated if available.
+	 * Handles an API request with no authorization.
 	 *
 	 * @param {(ctx: Context) => boolean} handler The API controller launched
 	 * @returns {(ctx: Context) => Promise<void>} A middleware function for Koa
 	 */
 	public anon(handler: (ctx: Context) => boolean) {
-		return this.auth(handler, null, null, []);
+		return async (ctx: Context) => await this.handleRequest(ctx, handler);
 	}
 
 	/**
@@ -74,248 +73,42 @@ export abstract class Api {
 
 		return async (ctx: Context) => {
 
+			// if authentication failed, abort.
+			if (ctx.state.authError) {
+				return this.handleError(ctx, ctx.state.authError);
+			}
+
+			// if this resource is a service resource, we don't need a user.
+			if (scopes && scope.isValid([Scope.SERVICE], scopes) && !resource && !permission && ctx.state.tokenType === 'application') {
+				return await this.handleRequest(ctx, handler);
+			}
+
 			try {
-				let user: User | null;
-				delete ctx.state.user;
 
-				const token = this.retrieveToken(ctx);
-
-				// app token?
-				if (/[0-9a-f]{32,}/i.test(token.value)) {
-
-					user = await this.authenticateWithAppToken(ctx, token, resource, permission, scopes);
-
-					// if authenticated as service resource, we're done here.
-					if (user === null) {
-						return await this.handleRequest(ctx, handler);
-					}
-
-				// Otherwise, assume it's a JWT.
-				} else {
-					user = await this.authenticateWithJwt(ctx, token);
+				// just to be sure (auth middleware loaded?)...
+				if (!ctx.state.user) {
+					throw new ApiError('Something went wrong, authenticated but no user.');
 				}
 
-				// make sure the authorization is given
-				await this.authorizeUser(ctx, user, resource, permission, scopes, planAttrs);
+				// now we're authenticated, let's authorize.
+				await this.authorizeUser(ctx, ctx.state.user, resource, permission, scopes, planAttrs);
 
-				// ---- here the user's authenticated and authorized. ---- //
-
-				// set dirty header if necessary
-				const result = await ctx.redis.getAsync('dirty_user_' + user.id);
+				// now we're authorized, set dirty header if necessary
+				const result = await ctx.redis.getAsync('dirty_user_' + ctx.state.user.id);
 				if (result) {
-					logger.info('[ctrl|auth] User <%s> is dirty, telling him in header.', user.email);
+					logger.info('[ctrl|auth] User <%s> is dirty, telling him in header.', ctx.state.user.email);
 					ctx.set('X-User-Dirty', result);
-					await ctx.redis.delAsync('dirty_user_' + user.id);
+					await ctx.redis.delAsync('dirty_user_' + ctx.state.user.id);
 				}
 				ctx.set('X-User-Dirty', '0');
 
-				// log to sqreen
-				if (config.vpdb.services.sqreen.enabled) {
-					require('sqreen').identify(ctx.req, { email: user.email });
-				}
-
-				// update state
-				ctx.state.user = user;
-
 			} catch (err) {
-				// only fail when not coming from anon
-				if (resource !== null || permission !== null) {
-					return this.handleError(ctx, err);
-				}
+				return this.handleError(ctx, err);
 			}
 
 			// continue with request
 			await this.handleRequest(ctx, handler);
 		};
-	}
-
-	/**
-	 * Retrieves the token from either URL or HTTP header.
-	 *
-	 * @param {Context} ctx Koa context
-	 * @returns {{token: string, fromUrl: boolean}} Token
-	 * @throws {ApiError} If no token found or incorrect header.
-	 */
-	private retrieveToken(ctx: Context): { value: string, fromUrl: boolean } {
-		const headerName = config.vpdb.authorizationHeader;
-		// get token
-		if (ctx.get(headerName.toLowerCase())) {
-
-			// validate format
-			const parts = ctx.get(headerName.toLowerCase()).split(' ');
-			if (parts.length !== 2) {
-				throw new ApiError('Bad Authorization header. Format is "%s: Bearer [token]"', headerName).status(401);
-			}
-			const scheme = parts[0];
-			const credentials = parts[1];
-			if (!/^Bearer$/i.test(scheme)) {
-				throw new ApiError('Bad Authorization header. Format is "%s: Bearer [token]"', headerName).status(401);
-			}
-			return {
-				value: credentials,
-				fromUrl: false
-			};
-
-		} else if (ctx.query && ctx.query.token) {
-			return {
-				value: ctx.query.token,
-				fromUrl: true
-			};
-		} else {
-			throw new ApiError('Unauthorized. You need to provide credentials for this resource').status(401);
-		}
-	}
-
-	/**
-	 * Tries to authenticate with a database token.
-	 *
-	 * For a "service resource" (a resource available to third-party services
-	 * without an authenticated user), `null` is returned in case of success.
-	 *
-	 * Otherwise, the authenticated user is returned.
-	 *
-	 * @param {Context} ctx Koa context
-	 * @param {{value: string, fromUrl: boolean}} token Retrieved token
-	 * @param {string} resource Resource, must be unset for service resources.
-	 * @param {string} permission Permission, must be unset for service resources.
-	 * @param {Scope[]} scopes Scopes, indicates if the resource is a service resource.
-	 * @returns {Promise<User | null>} Authenticated user on success or null on successful service resource authentication.
-	 * @throws {ApiError} If provided token is invalid.
-	 */
-	private async authenticateWithAppToken(ctx: Context, token: { value: string, fromUrl: boolean }, resource: string, permission: string, scopes: Scope[]): Promise<User | null> {
-
-		const vpdbUserIdHeader = 'x-vpdb-user-id';
-		const providerUserIdHeader = 'x-user-id';
-
-		// application access tokens aren't allowed in the url
-		if (token.fromUrl) {
-			throw new ApiError('App tokens must be provided in the header.').status(401);
-		}
-
-		const appToken = await ctx.models.Token.findOne({ token: token }).populate('_created_by').exec();
-
-		// fail if not found
-		if (!appToken) {
-			new ApiError('Invalid app token.').status(401);
-		}
-
-		// fail if incorrect plan
-		if (appToken.type === 'personal' && !(appToken._created_by as User).planConfig.enableAppTokens) {
-			new ApiError('Your current plan "%s" does not allow the use of app tokens. Upgrade or contact an admin.', (appToken._created_by as User).planConfig.id).status(401);
-		}
-
-		// fail if expired
-		if (appToken.expires_at.getTime() < new Date().getTime()) {
-			new ApiError('Token has expired.').status(401);
-		}
-
-		// fail if inactive
-		if (!appToken.is_active) {
-			new ApiError('Token is inactive.').status(401);
-		}
-
-		// so we're good here!
-		ctx.state.appToken = appToken;
-		ctx.state.tokenType = appToken.type;
-		ctx.state.tokenScopes = appToken.scopes;
-
-		let user: User;
-
-		// additional checks for application (provider) token
-		if (appToken.type === 'application') {
-
-			ctx.state.tokenProvider = appToken.provider;
-
-			// if this resource is a service resource, we don't need a user. But make sure no permissions needed.
-			if (scopes && scope.isValid([Scope.SERVICE], scopes) && !resource && !permission) {
-				await appToken.update({ last_used_at: new Date() });
-				return null;
-			}
-
-			// retrieve user id headers
-			const userId = ctx.get(vpdbUserIdHeader);
-			const providerUserId = ctx.get(providerUserIdHeader);
-
-			// vpdb user id header provided
-			if (userId) {
-				user = await ctx.models.User.findOne({ id: userId });
-				if (!user) {
-					throw new ApiError('No user with ID "%s".', userId).status(400);
-				}
-				if (!user.providers[appToken.provider]) {
-					throw new ApiError('Provided user has not been authenticated with %s.', appToken.provider).status(400);
-				}
-
-			// provider id header provided
-			} else if (providerUserId) {
-				user = await ctx.models.User.findOne({ ['providers.' + appToken.provider + '.id']: String(providerUserId) });
-
-				if (!user) {
-					throw new ApiError('No user with ID "%s" for provider "%s".', providerUserId, appToken.provider).status(400);
-				}
-
-			// if no user header found, fail.
-			} else {
-				throw new ApiError('Must provide "%s" or "%s" header when using application token.', vpdbUserIdHeader, providerUserIdHeader).status(400);
-			}
-
-		} else {
-			user = appToken._created_by as User;
-		}
-
-		await appToken.update({ last_used_at: new Date() });
-
-		return user;
-	}
-
-	/**
-	 * Tries to authenticate with a JSON Web Token.
-	 *
-	 * @param {Context} ctx Koa context
-	 * @param {{value: string, fromUrl: boolean}} token Retrieved token
-	 * @returns {Promise<User>} Authenticated user on success.
-	 * @throws {ApiError} If provided token is invalid.
-	 */
-	private async authenticateWithJwt(ctx: Context, token: { value: string, fromUrl: boolean }): Promise<User> {
-
-		// validate token
-		let decoded: Jwt;
-		try {
-			decoded = jwtDecode(token, config.vpdb.secret, false, 'HS256');
-		} catch (e) {
-			throw new ApiError(e, 'Bad JSON Web Token').status(401);
-		}
-
-		// check for expiration
-		let tokenExp = new Date(decoded.exp);
-		if (tokenExp.getTime() < new Date().getTime()) {
-			throw new ApiError('Token has expired').status(401);
-		}
-
-		if (token.fromUrl && !decoded.path) {
-			throw new ApiError('Tokens that are valid for any path cannot be provided as query parameter.').status(401);
-		}
-
-		// check for path && method
-		let extPath = settings.intToExt(ctx.request.path);
-		if (decoded.path && (decoded.path !== extPath || (ctx.method !== 'GET' && ctx.method !== 'HEAD'))) {
-			throw new ApiError('Token is only valid for "GET/HEAD %s" but got "%s %s".', decoded.path, ctx.method, extPath).status(401);
-		}
-
-		const user = await ctx.models.User.findOne({ id: decoded.iss });
-		if (!user) {
-			throw new ApiError('No user with ID %s found.', decoded.iss).status(403).log();
-		}
-
-		// generate new token if it's a short term token.
-		let tokenIssued = new Date(decoded.iat);
-		if (tokenExp.getTime() - tokenIssued.getTime() === config.vpdb.apiTokenLifetime) {
-			ctx.set('X-Token-Refresh', AuthenticationUtil.generateApiToken(user, new Date(), true));
-		}
-
-		ctx.state.tokenScopes = [Scope.ALL];
-		ctx.state.tokenType = decoded.irt ? 'jwt-refreshed' : 'jwt';
-		return user;
 	}
 
 	/**
@@ -366,12 +159,12 @@ export abstract class Api {
 				newVal = newObj[field] ? new Date(newObj[field]).getTime() : undefined;
 				oldVal = oldObj[field] ? new Date(oldObj[field]).getTime() : undefined;
 
-				// for objects, serialize first.
+			// for objects, serialize first.
 			} else if (isObject(oldObj[field])) {
 				newVal = newObj[field] ? JSON.stringify(newObj[field]) : undefined;
 				oldVal = oldObj[field] ? JSON.stringify(pick(oldObj[field], keys(newObj[field] || {}))) : undefined;
 
-				// otherwise, take raw values.
+			// otherwise, take raw values.
 			} else {
 				newVal = newObj[field];
 				oldVal = oldObj[field];

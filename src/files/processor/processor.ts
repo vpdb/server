3e7c36dbad07fddbbf585c163b5cf56b1ac5514b
freezy <freezy@kodi.tv>
2018-05-21 01:35:26 +0200

    refactor: Add documentation to abstract processor.

--- src/files/processor/processor.ts
+++ src/files/processor/processor.ts
@@ -23,12 +23,43 @@ import { FileVariation } from '../file.variations';
 
 export abstract class Processor<V extends FileVariation> {
 
+	/**
+	 * Checks whether the variation of the given file can be processed by this
+	 * processor.
+	 *
+	 * @param {File} file File to check
+	 * @param {FileVariation} variation Variation of the file to check, original if not set.
+	 * @returns {boolean} True if it can be processed, false otherwise.
+	 */
 	abstract canProcess(file: File, variation?: FileVariation): boolean;
 
+	/**
+	 * Returns the type of queue the processor should be run under.
+	 *
+	 * @returns {ProcessorQueue}
+	 */
 	abstract getQueue(): ProcessorQueue;
 
-	abstract getPriority(variation?: FileVariation): number;
+	/**
+	 * Returns a number indicating the order of execution of this processor.
+	 *
+	 * This is needed to define an order for multiple processors, but it can also
+	 * be used to prefer some variations to others.
+	 *
+	 * @param {FileVariation} variation Variation of the file to process, or original if not set.
+	 * @returns {number} Order of processing
+	 */
+	abstract getOrder(variation?: FileVariation): number;
 
+	/**
+	 * Starts processing the file. This is executed in Bull.
+	 *
+	 * @param {File} file File to process
+	 * @param {string} src Source path of the file
+	 * @param {string} dest Destination path
+	 * @param variation Variation to process
+	 * @returns {Promise<File>}
+	 */
 	abstract async process(file: File, src:string, dest:string, variation?: V): Promise<File>;
 }
 

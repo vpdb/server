1ebc32ae0b35b2f463cd0691d080590b95a0274a
freezy <freezy@kodi.tv>
2018-05-27 22:59:37 +0200

    refactor: Move misc resources to TS.

--- src/games/game.d.ts
+++ src/games/game.d.ts
@@ -5,16 +5,16 @@
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * of the License; or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful;
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * along with this program; if not; write to the Free Software
+ * Foundation; Inc.; 51 Franklin Street; Fifth Floor; Boston; MA  02110-1301; USA.
  */
 
 import { Document, Schema } from 'mongoose';
@@ -22,57 +22,58 @@ import { File } from '../files/file';
 import { User } from '../users/user';
 
 export interface Game extends Document {
-	id: string,
-	title: string,
-	title_sortable: string,
-	year: number,
-	manufacturer: string,
-	game_type: 'ss' | 'em' | 'pm' | 'og' | 'na',
-	_backglass: File | Schema.Types.ObjectId,
-	_logo: File | Schema.Types.ObjectId,
-	short: Array<string>,
-	description: string,
-	instructions: string,
-	produced_units: number,
-	model_number: string,
-	themes: Array<string>,
-	designers: Array<string>,
-	artists: Array<string>,
-	keywords: Array<string>,
-	features: string,
-	notes: string,
-	toys: string,
-	slogans: string,
-	ipdb: {
-		number: number,
-		rating: number,
-		rank: number,
-		mfg: number,
-		mpu: number
-	},
-	pinside: {
-		ids: string[],
-		ranks: number[],
-		rating: number
-	},
-	counter: {
-		releases: number,
-		views: number,
-		downloads: number,
-		comments: number,
-		stars: number,
-	},
-	metrics: {
-		popularity: number,
-	},
-	rating: {
-		average: number,
-		votes: number,
-		score: number,
-	},
-	modified_at: Date,
-	created_at: Date,
-	_created_by: User | Schema.Types.ObjectId;
+	id?: string;
+	title: string;
+	title_sortable?: string;
+	year: number;
+	manufacturer: string;
+	game_type: 'ss' | 'em' | 'pm' | 'og' | 'na';
+	_backglass?: File | Schema.Types.ObjectId;
+	_logo?: File | Schema.Types.ObjectId;
+	short?: string[];
+	description?: string;
+	instructions?: string;
+	produced_units?: number;
+	model_number?: string;
+	themes?: string[];
+	designers?: string[];
+	artists?: string[];
+	animators?: string[];
+	keywords?: string[];
+	features?: string;
+	notes?: string;
+	toys?: string;
+	slogans?: string;
+	ipdb?: {
+		number?: number;
+		rating?: number;
+		rank?: number;
+		mfg?: number;
+		mpu?: number
+	};
+	pinside?: {
+		ids?: string[];
+		ranks?: number[];
+		rating?: number
+	};
+	counter?: {
+		releases?: number;
+		views?: number;
+		downloads?: number;
+		comments?: number;
+		stars?: number;
+	};
+	metrics?: {
+		popularity?: number;
+	};
+	rating?: {
+		average?: number;
+		votes?: number;
+		score?: number;
+	};
+	modified_at?: Date;
+	created_at?: Date;
+	_created_by?: User | Schema.Types.ObjectId;
 
 	// serialized
 	backglass?: File;
@@ -80,7 +81,7 @@ export interface Game extends Document {
 
 	// generated
 	mpu?: number;
-	owner: string;
+	owner?: string;
 	restrictions?: GameRestrictions;
 
 	isRestricted(what:string):boolean;
@@ -93,5 +94,5 @@ export interface GameRestrictions {
 }
 
 export interface GameRestriction {
-	mpu: boolean;
+	mpu?: boolean;
 }
--- src/misc/misc.api.ts
+++ src/misc/misc.api.ts
@@ -17,11 +17,15 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+import { extend } from 'lodash';
+
 import { Api } from '../common/api';
 import { logger } from '../common/logger';
 import { config } from '../common/settings';
 import { Context } from '../common/types/context';
 import { gitInfo } from '../common/gitinfo';
+import { ipdb } from '../common/ipdb';
+import { roles } from '../common/acl';
 import { processorQueue } from '../files/processor/processor.queue';
 
 const pak = require('../../package.json');
@@ -40,9 +44,44 @@ export class MiscApi extends Api {
 			app_name: config.vpdb.name,
 			app_version: pak.version,
 			app_date: gitInfo.hasInfo() ? gitInfo.getLastCommit().lastCommitTime : undefined,
-			app_sha:  gitInfo.hasInfo() ? gitInfo.getLastCommit().SHA : undefined
+			app_sha: gitInfo.hasInfo() ? gitInfo.getLastCommit().SHA : undefined
 		};
 		return this.success(ctx, result, 200);
+	}
+
+	/**
+	 * Retrieves metadata from IPDB.org.
+	 *
+	 * @see GET /v1/ipdb/:id
+	 * @param {Context} ctx Koa context
+	 */
+	public async ipdbDetails(ctx: Context) {
+		const game = await ipdb.details(ctx.params.id, { offline: ctx.query.dryrun });
+		return this.success(ctx, game);
+	}
+
+	/**
+	 * Returns all available ACL roles.
+	 *
+	 * @see GET /v1/roles
+	 * @param {Application.Context} ctx Koa context
+	 */
+	public async roles(ctx: Context) {
+		return this.success(ctx, roles, 200);
+	}
+
+	/**
+	 * Returns all available plans
+	 *
+	 * @see GET /v1/plans
+	 * @param {Application.Context} ctx Koa context
+	 */
+	public async plans(ctx: Context) {
+		const plans:any[] = [];
+		config.vpdb.quota.plans.forEach(plan => {
+			plans.push(extend(plan, { name: plan.name || plan.id, is_default: plan.id === config.vpdb.quota.defaultPlan }));
+		});
+		return this.success(ctx, plans, 200);
 	}
 
 	/**

04591231f1b3c851961e502d3172a5bac2b37844
freezy <freezy@kodi.tv>
2018-05-27 22:59:37 +0200

    refactor: Move misc resources to TS.

--- src/common/acl.ts
+++ src/common/acl.ts
@@ -27,7 +27,7 @@ export const acl = new ACL(new ACL.redisBackend(state.redis, 'acl'));
 /**
  * Initializes the ACLs.
  */
-export async function init():Promise<void> {
+export async function init(): Promise<void> {
 
 	// permissions
 	await acl.allow([
@@ -53,7 +53,7 @@ export async function init():Promise<void> {
 			]
 		}, {
 			roles: 'contributor',
-			allows: [ { resources: 'roms', permissions: ['add', 'delete-own']  } ]
+			allows: [{ resources: 'roms', permissions: ['add', 'delete-own'] }]
 		}, {
 			roles: 'game-contributor',
 			allows: [
@@ -62,10 +62,10 @@ export async function init():Promise<void> {
 			]
 		}, {
 			roles: 'release-contributor',
-			allows: [ { resources: 'releases', permissions: ['auto-approve'] }, ]
+			allows: [{ resources: 'releases', permissions: ['auto-approve'] }]
 		}, {
 			roles: 'backglass-contributor',
-			allows: [ { resources: 'backglasses', permissions: ['auto-approve'] } ]
+			allows: [{ resources: 'backglasses', permissions: ['auto-approve'] }]
 		}, {
 			roles: 'member',
 			allows: [
@@ -91,13 +91,15 @@ export async function init():Promise<void> {
 		}
 	]);
 
-	await acl.addRoleParents('root', [ 'admin', 'moderator' ]);
-	await acl.addRoleParents('admin', [ 'member' ]);
-	await acl.addRoleParents('moderator', [ 'contributor' ]);
-	await acl.addRoleParents('contributor', [ 'game-contributor', 'release-contributor', 'backglass-contributor' ]);
-	await acl.addRoleParents('game-contributor', [ 'member' ]);
-	await acl.addRoleParents('release-contributor', [ 'member' ]);
-	await acl.addRoleParents('backglass-contributor', [ 'member' ]);
+	// this must reflect what's described below in roles.
+	await acl.addRoleParents('root', ['admin', 'moderator']);
+	await acl.addRoleParents('admin', ['member']);
+	await acl.addRoleParents('moderator', ['contributor']);
+	await acl.addRoleParents('contributor', ['game-contributor', 'release-contributor', 'backglass-contributor']);
+	await acl.addRoleParents('game-contributor', ['member']);
+	await acl.addRoleParents('release-contributor', ['member']);
+	await acl.addRoleParents('backglass-contributor', ['member']);
+
 	logger.info('[acl.init] Added permissions to roles.');
 	const users = await state.models.User.find({}).lean().exec();
 	logger.info('[acl.init] Applying ACLs to %d users...', users.length);
@@ -105,4 +107,55 @@ export async function init():Promise<void> {
 		await acl.addUserRoles(user.id, user.roles);
 	}
 	logger.info('[acl.init] ACLs applied.');
+}
+
+export const roles:Role[] = [
+	{
+		id: 'root',
+		name: 'root',
+		description: 'Super user. Can create, edit and delete everything including admins.',
+		parents: ['admin', 'moderator']
+	}, {
+		id: 'admin',
+		name: 'Administrator',
+		description: 'User administrator. Can edit users and roles. Inherits from member.',
+		parents: ['member']
+	}, {
+		id: 'moderator',
+		name: 'Moderator',
+		description: 'Can moderate and delete all entities. Inherits from contributor.',
+		parents: ['contributor']
+	}, {
+		id: 'contributor',
+		name: 'Contributor',
+		description: 'Can upload releases, backglasses and ROMs without approval, as well as add and edit games. Inherits from all other contributors.',
+		parents: ['game-contributor', 'release-contributor', 'backglass-contributor']
+	}, {
+		id: 'game-contributor',
+		name: 'Game Contributor',
+		description: 'Can add and edit game data. Inherits from member.',
+		parents: ['member']
+	}, {
+		id: 'release-contributor',
+		name: 'Release Contributor',
+		description: 'Can upload releases without approval. Inherits from member.',
+		parents: ['member']
+	}, {
+		id: 'backglass-contributor',
+		name: 'Backglass Contributor',
+		description: 'Can upload backglasses without approval. Inherits from member.',
+		parents: ['member']
+	}, {
+		id: 'member',
+		name: 'Member',
+		description: 'A registered member. This role every user should have. Removing results in having the same permissions as anonymous.',
+		parents: []
+	},
+];
+
+export interface Role {
+	id: string;
+	name: string;
+	description: string;
+	parents: string[];
 }

82fdb193278e003a532ada3e1d3adc028a5f43d5
freezy <freezy@kodi.tv>
2018-06-03 23:27:48 +0200

    refactor: Move star and release schema to TS.

---
+++ src/stars/index.ts
@@ -0,0 +1,46 @@
+/*
+ * VPDB - Virtual Pinball Database
+ * Copyright (C) 2018 freezy <freezy@vpdb.io>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import Application = require('koa');
+import mongoose, { Schema } from 'mongoose';
+import Router from 'koa-router';
+
+import { state } from '../state';
+import { EndPoint } from '../common/types/endpoint';
+import { starSchema } from './star.schema';
+import { Star } from './star';
+
+export class StarEndPoint implements EndPoint {
+
+	readonly name: string = 'Ratings API';
+
+	private readonly _schema: Schema;
+
+	constructor() {
+		this._schema = starSchema;
+	}
+
+	getRouter(): Router {
+		return null;
+	}
+
+	register(app: Application): void {
+		state.models.Star = mongoose.model<Star>('Star', this._schema);
+	}
+}
--- src/stars/star.schema.ts
+++ src/stars/star.schema.ts
@@ -1,6 +1,6 @@
 /*
- * VPDB - Visual Pinball Database
- * Copyright (C) 2016 freezy <freezy@xbmc.org>
+ * VPDB - Virtual Pinball Database
+ * Copyright (C) 2018 freezy <freezy@vpdb.io>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -16,32 +16,25 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+import { Schema } from 'mongoose';
 
-'use strict';
-
-const logger = require('winston');
-const mongoose = require('mongoose');
-
-const Schema = mongoose.Schema;
 
 //-----------------------------------------------------------------------------
 // SCHEMA
 //-----------------------------------------------------------------------------
-const fields = {
-	_from: { type: Schema.ObjectId, required: true, ref: 'User', index: true },
+
+export const starFields = {
+	_from: { type: Schema.Types.ObjectId, required: true, ref: 'User', index: true },
 	_ref: {
-		game: { type: Schema.ObjectId, ref: 'Game', index: true, sparse: true },
-		release: { type: Schema.ObjectId, ref: 'Release', index: true, sparse: true },
-		user: { type: Schema.ObjectId, ref: 'User', index: true, sparse: true },
-		medium: { type: Schema.ObjectId, ref: 'Medium', index: true, sparse: true },
-		backglass: { type: Schema.ObjectId, ref: 'Backglass', index: true, sparse: true }
+		game: { type: Schema.Types.ObjectId, ref: 'Game', index: true, sparse: true },
+		release: { type: Schema.Types.ObjectId, ref: 'Release', index: true, sparse: true },
+		user: { type: Schema.Types.ObjectId, ref: 'User', index: true, sparse: true },
+		medium: { type: Schema.Types.ObjectId, ref: 'Medium', index: true, sparse: true },
+		backglass: { type: Schema.Types.ObjectId, ref: 'Backglass', index: true, sparse: true }
 	},
 	type: { type: String, 'enum': ['game', 'release', 'user', 'medium', 'backglass'], required: true, index: true },
 	created_at: { type: Date, required: true }
 };
 
-const StarSchema = new Schema(fields, { usePushEach: true });
+export const starSchema = new Schema(starFields, { toObject: { virtuals: true, versionKey: false } });
 // TODO autoindex: false in production: http://mongoosejs.com/docs/guide.html#indexes
-
-mongoose.model('Star', StarSchema);
-logger.info('[model] Schema "Star" registered.');

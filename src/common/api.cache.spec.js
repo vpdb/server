a1c5a73aae055fb31428801427440c1899d9abe8
freezy <freezy@kodi.tv>
2018-06-10 00:50:41 +0200

    test: Fix user on setup.

--- src/common/api.cache.spec.js
+++ src/common/api.cache.spec.js
@@ -39,7 +39,7 @@ describe('The VPDB API cache', () => {
 		});
 		release = await releaseHelper.createRelease('moderator');
 		otherRelease = await releaseHelper.createRelease('moderator');
-		await api.as('administrator').del('/v1/cache').then(res => res.expectStatus(204));
+		await api.as('admin').del('/v1/cache').then(res => res.expectStatus(204));
 	});
 
 	afterEach(async () => await api.as('admin').del('/v1/cache').then(res => res.expectStatus(204)));

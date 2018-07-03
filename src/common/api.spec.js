f4fde4c9af93718169c8efd2654422aeeec9244f
freezy <freezy@kodi.tv>
2018-07-04 00:05:33 +0200

    test: Add coverage for misc resources.

--- src/common/api.spec.js
+++ src/common/api.spec.js
@@ -1,10 +1,20 @@
 'use strict';
 /*global describe, before, after, beforeEach, afterEach, it*/
+const expect = require('expect.js');
 
 const ApiClient = require('../../test/modules/api.client');
 const api = new ApiClient();
 
+let res;
 describe('The VPDB API', () => {
+
+	before(async () => {
+		await api.setupUsers({
+			admin: { roles: ['admin'] },
+		});
+	});
+
+	after(async () => await api.teardown());
 
 	it('should return a HTTP 415 if anything else than JSON is posted', async () => {
 		await api
@@ -29,5 +39,31 @@ describe('The VPDB API', () => {
 			.withHeader('Content-Type', '')
 			.post('/v1/tags', {})
 			.then(res => res.expectError(415, 'You need to set the "Content-Type" header'));
+	});
+
+	it('should list the API version', async () => {
+		res = await api
+			.get('/v1')
+			.then(res => res.expectStatus(200));
+		expect(res.data.app_name).to.be.ok();
+		expect(res.data.app_version).to.be.ok();
+		expect(res.data.app_date).to.be.ok();
+		expect(res.data.app_sha).to.be.ok();
+	});
+
+	it('should list all plans', async () => {
+		res = await api
+			.get('/v1/plans')
+			.then(res => res.expectStatus(200));
+		expect(res.data.length).to.be(4);
+	});
+
+	it('should list all roles', async () => {
+		res = await api
+			.as('admin')
+			.get('/v1/roles')
+			.then(res => res.expectStatus(200));
+		const roles = res.data;
+		expect(roles.length).to.be(8);
 	});
 });

c0b4b4273b84a5956639b2d3344f015f5fcda8d8
freezy <freezy@kodi.tv>
2018-08-01 20:41:32 +0200

    chore: Use MongoDB port from env if set.

--- config/settings.protractor.js
+++ config/settings.protractor.js
@@ -13,7 +13,7 @@ module.exports = {
 			'protected': { path: './data/storage-test-protected', api: { protocol: 'http', hostname: 'localhost', port: 7357, pathname: '/storage', prefix: '' } }
 		},
 		webapp: { protocol: 'http', hostname: 'localhost', port: 3333 },
-		db: 'mongodb://localhost:27017/vpdb-test',
+		db: `mongodb://localhost:${process.env.MONGODB_PORT || 27017}/vpdb-test`,
 		redis: { host: '127.0.0.1', port: 6379, db: 7 },
 		apiTokenLifetime: 3600000,
 		storageTokenLifetime: 60000,

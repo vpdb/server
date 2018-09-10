93db434cf191935a1de518fd83cd3634fe4b376a
freezy <freezy@kodi.tv>
2018-09-10 23:30:07 +0200

    chore: Add migration script that clears cache.

--- src/app/common/api.cache.ts
+++ src/app/common/api.cache.ts
@@ -612,12 +612,12 @@ class ApiCache {
 	private async deleteWildcard(wildcard: string): Promise<number> {
 		const num = await state.redis.eval(
 			'local keysToDelete = redis.call(\'keys\', ARGV[1]) ' + // find keys with wildcard
-			'if unpack(keysToDelete) ~= nil then ' +              // if there are any keys
-			'return redis.call(\'del\', unpack(keysToDelete)) ' +   // delete all
+			'if unpack(keysToDelete) ~= nil then ' +                     // if there are any keys
+			'return redis.call(\'del\', unpack(keysToDelete)) ' +        // delete all
 			'else ' +
-			'return 0 ' +                                         // if no keys to delete
+			'return 0 ' +                                                // if no keys to delete
 			'end ',
-			0,                                                    // no keys names passed, only one argument ARGV[1]
+			0,                                                           // no keys names passed, only one argument ARGV[1]
 			wildcard);
 		return num as number;
 	}

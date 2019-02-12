fa61e0e73f67ed7adf208f0fab7f70db749d1256
freezy <freezy@kodi.tv>
2019-02-12 23:26:45 +0100

    style: Welcome to 2019!

--- src/app/common/serializer.ts
+++ src/app/common/serializer.ts
@@ -1,6 +1,6 @@
 /*
  * VPDB - Virtual Pinball Database
- * Copyright (C) 2018 freezy <freezy@vpdb.io>
+ * Copyright (C) 2019 freezy <freezy@vpdb.io>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License

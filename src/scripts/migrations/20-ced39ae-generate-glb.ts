2074543d71258bbf9c5b5d03f6152a7fe8087e2e
freezy <freezy@kodi.tv>
2019-04-11 00:03:36 +0200

    migration: Update migration script to only update metadata when file is available.

--- src/scripts/migrations/20-ced39ae-generate-glb.ts
+++ src/scripts/migrations/20-ced39ae-generate-glb.ts
@@ -17,26 +17,57 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+import { stat } from 'fs';
+import { promisify } from 'util';
+
 import { apiCache } from '../../app/common/api.cache';
 import { logger } from '../../app/common/logger';
+import { FileUtil } from '../../app/files/file.util';
 import { processorQueue } from '../../app/files/processor/processor.queue';
 import { state } from '../../app/state';
 
+const statAsync = promisify(stat);
+
 /**
- * Re-generates al GLB files.
+ * Re-generates all missing GLB files or updates metadata for existing ones.
  */
 export async function up() {
 
 	// create GLB variations
 	const mimeType = 'application/x-visual-pinball-table-x';
+	const variationName = 'gltf';
 	const files = await state.models.File.find({ mime_type : mimeType }).exec();
 	for (const file of files) {
-		await processorQueue.reprocessFile(null, file, false,
-			variation => variation.name === 'gltf',
-			() => false);
+
+		const variation = file.getVariation(variationName);
+		const destPath = file.getPath(null, variation);
+
+		// if file exists, just update metadata
+		if (await FileUtil.exists(destPath)) {
+			logger.info(null, '[migrate-20] Updating metadata for file %s', destPath);
+
+			// update metadata
+			const commonMetadata: any = {
+				bytes: (await statAsync(destPath)).size,
+				mime_type: variation.mimeType,
+			};
+			const fileData = { [`variations.${variation.name}`]: commonMetadata };
+			await state.models.File.findOneAndUpdate({ _id: file._id }, { $set: fileData }).exec();
+
+		// otherwise re-process
+		} else {
+			logger.info(null, '[migrate-20] Reprocessing file at %s...', destPath);
+			try {
+				await processorQueue.reprocessFile(null, file, false,
+					v => v.name === variationName,
+					() => false);
+			} catch (err) {
+				logger.error(null, '[migrate-20] Failed reprocessing: %s', err.message);
+			}
+		}
 	}
 
 	// clear all caches
 	await apiCache.invalidateAll();
-	logger.info(null, '[migrate-19] Cache invalidated.');
+	logger.info(null, '[migrate-20] Cache invalidated.');
 }
